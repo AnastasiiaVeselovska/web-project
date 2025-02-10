561b776719cc35b93d811c0b50354e9cb3c7a6a0
Anastasiia Veselovska <veselovskaaanastasia03@gmail.com>
2025-02-10 16:55:25 +0200

    Added new js and fixed bugs

--- js/contacts.faq_content.js
+++ js/contacts.faq_content.js
@@ -8,7 +8,7 @@ faqQuestions.forEach((question) => {
     if (answer.style.maxHeight) {
       answer.style.maxHeight = null;
     } else {
-      answer.style.maxHeight = answer.scrollHeight + "px"; 
+      answer.style.maxHeight = answer.scrollHeight + "px";
     }
 
     faqQuestions.forEach((otherQuestion) => {
@@ -19,4 +19,3 @@ faqQuestions.forEach((question) => {
     });
   });
 });
-
